5e2136b3bcfde9e75344be12708ed4acd84afa2d
Eltaurus-LT02 <Eltaurus@mail.ru>
2024-06-13 21:06:26 +0600

    select video file dialog

--- Dialogs.py
+++ Dialogs.py
@@ -35,6 +35,12 @@ class FillChoices(QDialog):
         self.button_ok = QPushButton("OK")
         self.button_cancel = QPushButton("Cancel")
 
+        self.videoFile_label = QLabel("Video")
+        self.videoFile_path = QLineEdit("")
+        self.videoFile_path.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.videoFile_button = QPushButton("Select File", self)
+        self.videoFile_button.clicked.connect(self.selectVideoFile)
+
         self.subtitleFile_label = QLabel("Subtitles")
         self.subtitleFile_path = QLineEdit("")
         self.subtitleFile_path.setAlignment(Qt.AlignmentFlag.AlignRight)
@@ -60,6 +66,20 @@ class FillChoices(QDialog):
         layout.addLayout(radio_layout)
 
         layout.addSpacing(2 * font_size)
+
+        layout.addWidget(self.videoFile_label)
+        videoFile_layout = QHBoxLayout()
+        videoFile_layout.addWidget(self.videoFile_path)
+        videoFile_layout.addWidget(self.videoFile_button)
+        layout.addLayout(videoFile_layout)
+
+        layout.addWidget(self.subtitleFile_label)
+        subtitleFile_layout = QHBoxLayout()
+        subtitleFile_layout.addWidget(self.subtitleFile_path)
+        subtitleFile_layout.addWidget(self.subtitleFile_button)
+        layout.addLayout(subtitleFile_layout)
+
+        layout.addSpacing(2 * font_size)
         layout.addStretch()
   
         button_layout = QHBoxLayout()
@@ -67,12 +87,6 @@ class FillChoices(QDialog):
         button_layout.addWidget(self.button_ok)
         button_layout.addWidget(self.button_cancel)
         layout.addLayout(button_layout)
-
-        layout.addWidget(self.subtitleFile_label)
-        subtitleFile_layout = QHBoxLayout()
-        subtitleFile_layout.addWidget(self.subtitleFile_path)
-        subtitleFile_layout.addWidget(self.subtitleFile_button)
-        layout.addLayout(subtitleFile_layout)
 
         # Set size limits
         self.setMinimumWidth(30 * font_size)
@@ -89,19 +103,23 @@ class FillChoices(QDialog):
     def get_selected_options(self):
         return self.source_field.currentText(), self.choices_field.currentText(), self.radio_append.isChecked()
 
+    def selectVideoFile(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setWindowTitle("Select the video file")
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        file_dialog.setNameFilter("Video Files (*.mkv *.mp4 .*m2ts *.avi);;All Files (*)")
+
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()
+            if selected_files:
+                file_name = selected_files[0]
+                self.videoFile_path.setText(file_name)
+
     def selectSubtitleFile(self):
         file_dialog = QFileDialog(self)
         file_dialog.setWindowTitle("Select the file with subtitles")
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
         file_dialog.setNameFilter("Subtitles (*.srt *.sub *.ass *.stl);;Text (*.txt);;All Files (*)")
-
-        # options = QFileDialog.Options()
-        # options |= QFileDialog.DontUseNativeDialog
-        # file_name, _ = QFileDialog.getOpenFileName(self,
-        #                     "Select the file with subtitles",
-        #                     "",
-        #                     "Subtitles (*.srt *.sub *.ass *.stl);;Text (*.txt);;All Files (*)",
-        #                     options=options)
 
         if file_dialog.exec():
             selected_files = file_dialog.selectedFiles()
