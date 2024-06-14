841111b911e7c92b7d58209290d0edd7cb35ff28
Eltaurus-LT02 <Eltaurus@mail.ru>
2024-06-14 16:22:16 +0600

    make screenshots with ffmpeg | closes #6 | closes #7

--- Fill_Context_menu.py
+++ Fill_Context_menu.py
@@ -2,6 +2,11 @@ from . import Dialogs
 from aqt import mw, gui_hooks
 from aqt.qt import *
 from aqt.utils import tooltip
+import os
+overwrite = True
+
+def averageTimestamps(t1, t2):
+    return
 
 def subtitleParse(filepath):
     return [{'ts': 42, 'sentence': "ピクニックに行く"}]
@@ -18,9 +23,11 @@ def formatSampleSentence(word_form, sentence):
     return sentence
 
 def composeScreenshotFilename(source_filename, t):
-    return "screenshot00.png"
+    return "screenshot00.jpg"
 
 def saveScreenshot(ts, saveas, videoSource):
+    fullOutputPath = os.path.join(mw.col.media.dir(), saveas)
+    os.popen(f'ffmpeg {"-y" if overwrite else "-n"} -ss {ts} -i \"{videoSource}\" -vf scale=720:-1 -frames:v 1 -q:v 7 \"{fullOutputPath}\"')
     return
 
 def contextualize(browser):
@@ -47,13 +54,13 @@ def contextualize(browser):
     counter = 0
     for note in notes:
 # add checks for non-existing fields and empty search results
-        word_conjugations = wordConjugations(note[word_field], note[word_conj_field], "", "")
-        searchResult = subtitleWordSearch(word_conjugations, sentence_db)
-        note[sentence_field] = formatSampleSentence(searchResult['word_form'], searchResult['sentence'])
-        screenshotFilename = composeScreenshotFilename(videoFile_path, searchResult['ts'])
-        screenshots_meta.add({'ts': searchResult['ts'], 'filename': screenshotFilename})
-        note[screenshot_field] = f"<img src='{screenshotFilename}'/>"
-        note[source_field] = source_text
+        # word_conjugations = wordConjugations(note[word_field], note[word_conj_field], "", "")
+        # searchResult = subtitleWordSearch(word_conjugations, sentence_db)
+        # note[sentence_field] = formatSampleSentence(searchResult['word_form'], searchResult['sentence'])
+        # screenshotFilename = composeScreenshotFilename(videoFile_path, searchResult['ts'])
+        # screenshots_meta.add({'ts': searchResult['ts'], 'filename': screenshotFilename})
+        # note[screenshot_field] = f"<img src='{screenshotFilename}'/>"
+        # note[source_field] = source_text
         counter += 1
 #<- save note
 
@@ -61,7 +68,11 @@ def contextualize(browser):
     for meta in screenshots_meta:
         saveScreenshot(meta['ts'], meta['filename'], videoFile_path)
 
-    tooltip(f'{sample_field}, {screenshot_field}, {source_field}, {source_text}, {videoFile_path}, {subtitleFile_path}')
+#    test_ts = "00:16:24.00"
+#    test_videoFile_path = "D:\Lang\日本語\聴解\shirokumakafe\Shirokuma Cafe e04.mkv"
+#    saveScreenshot(test_ts, composeScreenshotFilename(test_videoFile_path, test_ts), test_videoFile_path)
+
+    tooltip(f'{sentence_field}, {screenshot_field}, {source_field}, {source_text}, {videoFile_path}, {subtitleFile_path}')
     
 
 
