e500ed90574fe6a71af727a1fc80cc5d665d49df
Eltaurus <eltaurus@inbox.lt>
2025-01-26 19:40:11 +0600

    copyright notices (Eltaurus)
    Co-authored-by: zunios <zunios@mail.ru>

--- Screenshots.py
+++ Screenshots.py
@@ -1,3 +1,25 @@
+# This script is part of the Contextualize Add-on for Anki.
+# Source: https://github.com/Eltaurus-Lt/Anki-Contextualize
+# 
+# Copyright © 2024-2025 Eltaurus
+# Contact: 
+#     Email: Eltaurus@inbox.lt
+#     GitHub: github.com/Eltaurus-Lt
+#     Anki Forums: forums.ankiweb.net/u/Eltaurus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import os
 from aqt import mw
 
